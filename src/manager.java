5a1338ab6e7745d6ba2f95b3bfc2559ee00e957d
pengzhenghao <pengzh@ie.cuhk.edu.hk>
2022-04-04 22:35:20 +0800

    finish the read function

--- src/manager.java
+++ src/manager.java
@@ -3,6 +3,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
+import java.util.*;
+import java.sql.*;
 
 public class manager {
     private String jdbcDriver = "com.mysql.jdbc.Driver";
@@ -46,37 +48,75 @@ public class manager {
         return 1;
     }
 
-    public void rentCar() {
-
-        Scanner inputScanner = new Scanner(System.in);
-
-        // Enter user ID
+    private String readUserID(Statement stmt, Scanner inputScanner) {
         System.out.printf("Enter the User ID: ");
         String userID = inputScanner.next();
-        System.out.printf("Enter the Call Number: ");
-        String callNumber = inputScanner.next();
-        System.out.printf("Enter the Copy Number: ");
-        String copyNumber = inputScanner.next();
 
-        // Do job!
-        // Step 1: Then the system should check whether that car is available
-        // to be rented (i.e., There is no rent record of the specified car copy
-        // with NULL return date).
-        //
-        // Step 2: If the car copy is available, it is then borrowed and a new
-        // check-out record of the specified car copy and user with NULL
-        // return date should be added to the database accordingly.
-        //
-        // Step 3: Finally, there should be an informative message whether
-        // the car copy can be lent successfully in layman terms.
-        //
+        // Check whether the User ID is in the database
+        String sql;
+        sql = "SELECT uid, callnum, copynum, checkout, return_date" +
+                "FROM rent R" +
+                "WHERE R.uid=" + userID;
+        try {
+            ResultSet rs = stmt.executeQuery(sql);
+            rs.last();
+            if (rs.getRow() != 1){
+                System.out.println("The input User ID " + userID + " is not found in database!");
+                userID = "";
+            };
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
-
-
-        // Finish
-        System.out.println("Car renting performed successfully.");
+        return userID;
+    }
 
 
+    private String readCallNumber(Statement stmt, Scanner inputScanner) {
+        return "";
+    }
+    private String readCopyNumber(Statement stmt, Scanner inputScanner) {
+        return "";
+    }
+
+    public void rentCar() {
+        try {
+
+            // Setup connection
+            Class.forName(jdbcDriver);
+            Connection conn = DriverManager.getConnection(dbAddress, userName, password);
+            Statement stmt = conn.createStatement();
+
+            Scanner inputScanner = new Scanner(System.in);
+
+            String userID = this.readUserID(stmt, inputScanner);
+            String callNumber = this.readCallNumber(stmt, inputScanner);
+            String copyNumber = this.readCopyNumber(stmt, inputScanner);
+
+
+            // Do job!
+            // Step 1: Then the system should check whether that car is available
+            // to be rented (i.e., There is no rent record of the specified car copy
+            // with NULL return date).
+            //
+            // Step 2: If the car copy is available, it is then borrowed and a new
+            // check-out record of the specified car copy and user with NULL
+            // return date should be added to the database accordingly.
+            //
+            // Step 3: Finally, there should be an informative message whether
+            // the car copy can be lent successfully in layman terms.
+            //
+
+
+            // Finish
+            System.out.println("Car renting performed successfully.");
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
